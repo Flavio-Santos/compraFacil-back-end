961a25d505e70b06ccca9410ead432435c68c020
Ariel Vieira <arielvieira96@gmail.com>
2017-10-10 20:46:19 -0300

    post de categoria feito, e mapeamento de cidade e estado feito

--- src/main/java/com/compraFacil/domain/Estado.java
+++ src/main/java/com/compraFacil/domain/Estado.java
@@ -1,9 +1,15 @@
 package com.compraFacil.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Estado {
@@ -12,6 +18,10 @@ public class Estado {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy="estado")
+	private List<Cidade> cidades = new ArrayList<>();
 	
 	public Estado() {
 		
