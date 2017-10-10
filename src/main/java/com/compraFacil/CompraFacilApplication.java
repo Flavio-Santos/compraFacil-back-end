43ec2a31ab155fb433685e485c9d2c2466a80e41
Ariel Vieira <arielvieira96@gmail.com>
2017-10-10 15:36:04 -0300

    Projeto criado

---
+++ src/main/java/com/compraFacil/CompraFacilApplication.java
@@ -0,0 +1,12 @@
+package com.compraFacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompraFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompraFacilApplication.class, args);
+	}
+}
