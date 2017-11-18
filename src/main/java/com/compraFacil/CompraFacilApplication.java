5af755bccd2f46a155e01195d4b8217938c4ad9c
Flavio-Santos <flavio.santos.pro@gmail.com>
2017-11-18 13:33:32 -0200

    add alteracao

--- src/main/java/com/compraFacil/CompraFacilApplication.java
+++ src/main/java/com/compraFacil/CompraFacilApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.compraFacil.domain.Categoria;
+import com.compraFacil.domain.Localizacao;
 import com.compraFacil.domain.Produto;
+import com.compraFacil.domain.Usuario;
 import com.compraFacil.repositories.CategoriaRepository;
+import com.compraFacil.repositories.LocalizacaoRepository;
 import com.compraFacil.repositories.ProdutoRepository;
+
 
 @SpringBootApplication
 public class CompraFacilApplication implements CommandLineRunner{
@@ -19,7 +23,8 @@ public class CompraFacilApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+	@Autowired
+	private LocalizacaoRepository localizacaoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -36,6 +41,19 @@ public class CompraFacilApplication implements CommandLineRunner{
 		
 		Produto prod1 = new Produto(null, 249.0, "Bicleta azul", "vendo bicicleta show", cat1);
 		produtoRepository.save(Arrays.asList(prod1));
+		
+		
+		Localizacao loc1 = new Localizacao(null, "-13151351", "-12132132132", "Em frente ao Mc Donalds");
+		Localizacao loc2 = new Localizacao(null, "-48648648", "-6845684684", "Em frente ao Burguer King");
+		
+		localizacaoRepository.save(Arrays.asList(loc1, loc2));
+		
+		Usuario user1 = new Usuario(null, "flavio", "asdasd@gmail.com", "fsan", "afafoo");
+		Usuario user2 = new Usuario(null, "santana", "santana@hotmail.com", "saint", "opop777");
+		
+		
+		
+		
 	
 	}
 }
