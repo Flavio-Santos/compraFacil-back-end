89ee0b53bb4e21e806951a898207c59472c097e4
Flavio-Santos <flavio.santos.pro@gmail.com>
2017-11-22 22:22:21 -0200

    merge

---
+++ src/test/java/com/compraFacil/repository/VendaTest.java
@@ -0,0 +1,33 @@
+package com.compraFacil.repository;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.compraFacil.domain.Categoria;
+import com.compraFacil.domain.Localizacao;
+import com.compraFacil.domain.Produto;
+import com.compraFacil.domain.Usuario;
+import com.compraFacil.domain.Venda;
+
+
+public class VendaTest {
+	
+	@Test
+	public void testandoVenda(){
+		Usuario usuario1 = new Usuario(1, "Santos", "flavio1@gmail.com", "fsan50", "1234");
+		Usuario usuario2 = new Usuario(2, "Santos alves", "flavio2@gmail.com", "fsan51", "1234");
+		Usuario usuario3 = new Usuario(3, "Santos flavio", "flavio3@gmail.com", "fsan52", "1234");
+		
+		Localizacao loc1 = new Localizacao(1,"1321321", "321321","asda asdas asda");
+		Categoria tenis = new Categoria(1, "tenis");
+		
+		Produto prod = new Produto(1, 500.00, "asics", "tenis top", tenis);
+		
+		Venda venda = new Venda(1, usuario1, prod,loc1);
+		
+		assertEquals(new Integer(1), venda.getId());
+		
+		
+	}
+}
